ce9c1548d216e88d413f839e4f4aeaa7a25c6b67
navysummer <navysummer@yeah.net>
2020-07-02 19:16:40 +0800

    add

--- alarm/pages/hostgroups/hostgroups.js
+++ alarm/pages/hostgroups/hostgroups.js
@@ -1,20 +1,265 @@
 // pages/hostgroups/hostgroups.js
+const app = getApp()
+var common = require('../../libs/common.js')
+var baseurl = app.globalData.baseurl
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    hiddenhostmodal:true,
+    hostgroup_hosts:[],
+    hostgroups:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+	  let basicAuth = app.globalData.basicAuth
+    let flag = common.authenticate(app)
+    if(!flag){
+      wx.redirectTo({
+        url: '/pages/login/login'
+      })
+    }
+    try {
+      var alarm_params = wx.getStorageSync('alarm_params')
+      if(typeof(alarm_params)=='undefined'){
+        wx.showModal({
+          title: '提示',
+          content: '获取alarm_params失败,即将返回前一页'
+        })
+        wx.redirectTo({
+          url: '/pages/index/index'
+        })
+      }else{
+        if(typeof(alarm_params.id)!='undefined' && typeof(alarm_params.region_name)!='undefined'){
+          let params = {
+            'config':{
+              'id':alarm_params.id,
+              'region_name':alarm_params.region_name
+            },
+            'params':{
+              "output": "extend"
+            }
+          }
+
+          if(typeof(alarm_params.params)!='undefined'){
+            params.params=alarm_params.params
+          }
+          if(typeof(options.groupid)!='undefined'){
+          	params.params.groupids = options.groupid
+          }
+          let _this = this
+          wx.request({
+            url: baseurl+'/hostgroups',
+            method:'POST',
+            header:{
+              "Content-Type": " application/json",
+              "Authorization": basicAuth
+            },
+            data:params,
+            success(res){
+              if(res.statusCode==200){
+                _this.setData({hostgroups:res.data})
+              }else{
+                console.log(res)
+                wx.showModal({
+                  title: '提示',
+                  content: res.data.error
+                })
+              }
+            },
+            fail(e){
+              console.log(e)
+              wx.showModal({
+                title: '提示',
+                content: '获取告警失败'
+              })
+            }
+          })
+        }
+      }
+    } catch (e) {
+      console.log(e)
+      wx.showModal({
+        title: '提示',
+        content: '获取alarm_params失败'
+      })
+    }
+  },
+  showhostgroup_host:function(e){
+    // console.log(e)
+    this.setData({hiddenhostmodal:false})
+    let groupid = e.currentTarget.dataset.groupid
+	  let basicAuth = app.globalData.basicAuth
+    let flag = common.authenticate(app)
+    if(!flag){
+      wx.redirectTo({
+        url: '/pages/login/login'
+      })
+    }
+    try {
+      var alarm_params = wx.getStorageSync('alarm_params')
+      if(typeof(alarm_params)=='undefined'){
+        wx.showModal({
+          title: '提示',
+          content: '获取alarm_params失败,即将返回前一页'
+        })
+        wx.redirectTo({
+          url: '/pages/index/index'
+        })
+      }else{
+        if(typeof(alarm_params.id)!='undefined' && typeof(alarm_params.region_name)!='undefined'){
+          let params = {
+            'config':{
+              'id':alarm_params.id,
+              'region_name':alarm_params.region_name
+            },
+            'params':{
+              "output": ["hostid","host"]
+            }
+          }
+
+          if(typeof(alarm_params.params)!='undefined'){
+            params.params=alarm_params.params
+          }
+          params.params.groupids = groupid
+          let _this = this
+          wx.request({
+            url: baseurl+'/hosts',
+            method:'POST',
+            header:{
+              "Content-Type": " application/json",
+              "Authorization": basicAuth
+            },
+            data:params,
+            success(res){
+              if(res.statusCode==200){
+                _this.setData({hostgroup_hosts:res.data})
+              }else{
+                console.log(res)
+                wx.showModal({
+                  title: '提示',
+                  content: res.data.error
+                })
+              }
+            },
+            fail(e){
+              console.log(e)
+              wx.showModal({
+                title: '提示',
+                content: '获取告警失败'
+              })
+            }
+          })
+        }
+      }
+    } catch (e) {
+      console.log(e)
+      wx.showModal({
+        title: '提示',
+        content: '获取alarm_params失败'
+      })
+    }
 
   },
+  hide:function(e){
+    this.setData({hiddenhostmodal:true})
+  },
+  formSubmit:function(e){
+    // console.log(e)
+    this.setData({hostgroups:[]})
+    let formData = e.detail.value
+    let hostgroups = this.trimArray(formData.hostgroups.split(','))
+    let basicAuth = app.globalData.basicAuth
+    let flag = common.authenticate(app)
+    if(!flag){
+      wx.redirectTo({
+        url: '/pages/login/login'
+      })
+    }
+    try {
+      var alarm_params = wx.getStorageSync('alarm_params')
+      if(typeof(alarm_params)=='undefined'){
+        wx.showModal({
+          title: '提示',
+          content: '获取alarm_params失败,即将返回前一页'
+        })
+        wx.redirectTo({
+          url: '/pages/index/index'
+        })
+      }else{
+        if(typeof(alarm_params.id)!='undefined' && typeof(alarm_params.region_name)!='undefined'){
+          let params = {
+            'config':{
+              'id':alarm_params.id,
+              'region_name':alarm_params.region_name
+            },
+            'params':{
+              "output": "extend"
+            }
+          }
 
+          if(typeof(alarm_params.params)!='undefined'){
+            params.params=alarm_params.params
+          }
+          if(hostgroups.length){
+          	params.params.filter = {"name":hostgroups}
+          }
+          let _this = this
+          wx.request({
+            url: baseurl+'/hostgroups',
+            method:'POST',
+            header:{
+              "Content-Type": " application/json",
+              "Authorization": basicAuth
+            },
+            data:params,
+            success(res){
+              if(res.statusCode==200){
+                _this.setData({hostgroups:res.data})
+              }else{
+                console.log(res)
+                wx.showModal({
+                  title: '提示',
+                  content: res.data.error
+                })
+              }
+            },
+            fail(e){
+              console.log(e)
+              wx.showModal({
+                title: '提示',
+                content: '获取告警失败'
+              })
+            }
+          })
+        }
+      }
+    } catch (e) {
+      console.log(e)
+      wx.showModal({
+        title: '提示',
+        content: '获取alarm_params失败'
+      })
+    }
+  },
+  formReset:function(e){
+    console.log(e)
+  },
+  trimArray:function(arr){
+    let newarr=[]
+    let len = arr.length
+    for(let i=0;i<len;i++){
+      if(arr[i]){
+        newarr.push(arr[i])
+      }
+    }
+    return newarr
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
--- alarm/pages/triggers/triggers.js
+++ alarm/pages/triggers/triggers.js
@@ -1,18 +1,108 @@
 // pages/triggers/triggers.js
+const app = getApp()
+var common = require('../../libs/common.js')
+var baseurl = app.globalData.baseurl
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    triggers:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    let basicAuth = app.globalData.basicAuth
+    let flag = common.authenticate(app)
+    if(!flag){
+      wx.redirectTo({
+        url: '/pages/login/login'
+      })
+    }
+    try {
+      var alarm_params = wx.getStorageSync('alarm_params')
+      if(typeof(alarm_params)=='undefined'){
+        wx.showModal({
+          title: '提示',
+          content: '获取alarm_params失败,即将返回前一页'
+        })
+        wx.redirectTo({
+          url: '/pages/index/index'
+        })
+      }else{
+        if(typeof(alarm_params.id)!='undefined' && typeof(alarm_params.region_name)!='undefined'){
+          let params = {
+            'config':{
+              'id':alarm_params.id,
+              'region_name':alarm_params.region_name
+            },
+            'params':{
+              "output": [
+                'triggerid',
+                'expression',
+                'description',
+                'status',
+                'state',
+                'value',
+                'comments',
+                'priority',
+                'lastchange'
+              ],
+              "expandComment": "extend",
+              "expandDescription": "extend",
+              "expandExpression": "extend",
+            }
+          }
 
+          if(typeof(alarm_params.params)!='undefined'){
+            params.params=alarm_params.params
+          }
+          if(typeof(options.hostid)!='undefined'){
+          	params.params.hostids = options.hostid
+          }
+          if(typeof(options.triggerid)!='undefined'){
+            params.params.triggerids = options.triggerid
+          }
+          let _this = this
+          wx.request({
+            url: baseurl+'/triggers',
+            method:'POST',
+            header:{
+              "Content-Type": " application/json",
+              "Authorization": basicAuth
+            },
+            data:params,
+            success(res){
+              if(res.statusCode==200){
+                _this.setData({triggers:res.data})
+              }else{
+                console.log(res)
+                wx.showModal({
+                  title: '提示',
+                  content: res.data.error
+                })
+              }
+            },
+            fail(e){
+              console.log(e)
+              wx.showModal({
+                title: '提示',
+                content: '获取告警失败'
+              })
+            }
+          })
+        }
+      }
+    } catch (e) {
+      console.log(e)
+      wx.showModal({
+        title: '提示',
+        content: '获取alarm_params失败'
+      })
+    }
   },
 
   /**
